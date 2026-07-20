1c72bca259fc0ab8939f2f8bf69dd961c8447902
zzckyy <ZackyMM2080@gmail.com>
2026-07-20 12:11:29 +0700

    feat: implement data loading functionality from Supabase and add table structure in program.html

---
+++ scripts/database.js
@@ -0,0 +1,26 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+  'https://laerdxalhjtclmxcwctm.supabase.co',
+  'sb_publishable_L8CUZH0KvYecouU_wEuYFg_ZvlJ6X5b'
+)
+
+async function loadData() {
+  const { data, error } = await supabase
+    .from('murid')
+    .select('*')
+
+  const table = document.getElementById('tableBody')
+
+  data.forEach(item => {
+    table.innerHTML += `
+      <tr>
+        <td>${item.nama}</td>
+        <td>${item.mapel}</td>
+        <td>${item.jadwal}</td>
+      </tr>
+    `
+  })
+}
+
+loadData()
